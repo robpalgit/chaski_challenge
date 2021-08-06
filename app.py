ad7cf215266ad8157b1caea9648384978a5ea203
Rob Pal <52180727+robpalgit@users.noreply.github.com>
2021-08-06 11:01:14 -0400

    commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import os
+import utils
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def dash():
+    if request.method == "POST":
+        file = request.files["csvfile"]
+        if not os.path.isdir("static"):
+            os.mkdir("static")
+        filepath = os.path.join("static", file.filename)
+        file.save(filepath)
+
+        resampled_df = utils.generate_resampled_data(filepath)
+
+        lineplot_path = utils.generate_lineplot(resampled_df)
+        histogram_path = utils.generate_histogram(resampled_df)
+        piechart_path = utils.generate_piechart(resampled_df)
+
+        return render_template(
+            "dash.html", 
+            lineplot=lineplot_path,
+            histogram=histogram_path,
+            piechart=piechart_path,
+             **{'SERVICE_URL': os.environ['SERVICE_URL']}
+             )
+
+    return render_template(
+        "home.html", 
+        **{'SERVICE_URL': os.environ['SERVICE_URL']}
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+plt.style.use("bmh")
+#plt.rcParams['figure.dpi'] = 72 #by default
+import seaborn as sb
+
+import os
+
+
+def determine_zone(bpm):
+    if (bpm > 0) & (bpm <= 18):
+        return "1 (Low)"
+    elif (bpm > 18) & (bpm <= 28):
+        return "2 (Medium)"
+    elif (bpm > 28) & (bpm <= 40):
+        return "3 (High)"
+    elif (bpm > 40):
+        return "4 (Extreme)"
+    
+
+def generate_resampled_data(filepath):
+    df = pd.read_csv(filepath, skiprows=1, header=1)
+
+    # Convert dateTime
+    df["dateTime"] = pd.to_datetime(df["dateTime"], unit="ms")
+
+    # Convert timeSeconds to time
+    df["time"] = pd.to_datetime(df["timeSeconds"], unit="s")
+
+    # Drop unnecessary columns
+    df.drop(["timeSeconds", "tempOral", "tempNasal", "signalPeriodSec", "dateTime"], axis=1, inplace=True)
+
+    # Set time as index
+    df.set_index("time", inplace=True)
+
+    resample_qty = 5
+    resample_bin = '{}S'.format(resample_qty)
+
+    resampled_df = df["signalFrequencyBpm"].resample(resample_bin).mean().reset_index()
+    resampled_df["time"] = resampled_df["time"].dt.time
+    resampled_df.set_index("time", inplace=True)
+
+    # Add new categorical variable 
+    resampled_df["bpmZone"] = resampled_df["signalFrequencyBpm"].apply(lambda x: determine_zone(x))
+
+    return resampled_df
+
+
+def generate_lineplot(resampled_df):
+    # Line plot
+    plt.figure(figsize=(10,2))
+    resampled_df["signalFrequencyBpm"].plot(color="blue")
+    #plt.xticks(rotation=45, ha="right")
+    # Save plot
+    lineplot_path = os.path.join("static", "lineplot" + ".png")
+    plt.savefig(lineplot_path)
+    return lineplot_path
+
+
+def generate_histogram(resampled_df):
+    # Histogram
+    plt.figure(figsize=(4,3))
+    sb.histplot(x="signalFrequencyBpm", data=resampled_df, bins=np.arange(12,34,1))
+    histogram_path = os.path.join("static", "histogram" + ".png")
+    plt.savefig(histogram_path)
+    return histogram_path
+
+
+def generate_piechart(resampled_df):
+    data = np.unique(resampled_df["bpmZone"], return_counts=True)
+    labels = data[0]
+    n_cats = len(labels)
+    
+    plt.figure(figsize=(4,3))
+    plt.pie(
+        x=data[1],
+#         labels=labels,
+        radius=0.9,
+        colors=sb.color_palette('hls', n_cats),
+        startangle=90,
+#         explode=(0,0,0,0.2,0,0,0.1),
+        autopct='%1.1f%%',
+        labeldistance=1.2,
+        textprops={"size": 14}
+    )
+    plt.title("Zones", size=16)
+    plt.legend(loc='best', labels=labels)
+
+    piechart_path = os.path.join("static", "piechart" + ".png")
+    plt.savefig(piechart_path)
+    return piechart_path
